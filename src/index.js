235c81407fe78ea8559e8f418f6dc31d5dbab90c
Helen Carolini <carolinihelen@hotmail.com>
2022-10-19 23:08:50 -0300

    Alterção de fonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+import "../src/assets/fontes/GandhiSans-Regular.otf"
+import "../src/assets/fontes/GandhiSans-Bold.otf"
+
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
